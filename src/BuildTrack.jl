76075bb9133535b8bffb41bab38f11d019e822ce
kobied <kedelman@gmail.com>
2022-05-26 20:52:00 +0300

    add pkg and update BuildTrack

--- src/BuildTrack.jl
+++ src/BuildTrack.jl
@@ -1,3 +1,20 @@
 module BuildTrack
+
+using Configurations, Colors
+
+const CONFIG_FILE = joinpath(@__DIR__,"../config/game.toml")
+
+
+@option struct WindowConfigs
+    HEIGHT = 400
+    WIDTH = 400
+    BACKGROUND = "purple"
+end
+
+@option struct GameConfigs
+    window::WindowConfigs
+end
+
+game_config = from_toml(GameConfigs, CONFIG_FILE)
     
 end
