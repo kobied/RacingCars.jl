3023e3e1ec2256ce0c19dce665a52ef37bcdb73f
Documenter.jl <documenter@juliadocs.github.io>
2022-05-26 16:58:45 +0000

    build based on cceb287

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = RacingCars","category":"page"},{"location":"#RacingCars","page":"Home","title":"RacingCars","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for RacingCars.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [RacingCars]","category":"page"}]
+}
